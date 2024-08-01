f3ac8c0f2904fa33d5fad168e55186f69da7ad46
claudiamacea <claudiamacea@gmail.com>
2024-08-01 23:17:56 +0300

    Logging. Created RequestResponseLogFilter class to intercept /api/v1 requests and responses for logging. Logging includes following info: HTTP method, request URI, content-type, response status.

---
+++ src/main/java/com/claudiamacea/store_management/filter/RequestResponseLogFilter.java
@@ -0,0 +1,41 @@
+package com.claudiamacea.store_management.filter;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RequestResponseLogFilter  extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        if (request.getRequestURI().toLowerCase().contains("/api/v1")) {
+            try {
+                filterChain.doFilter(request, response);
+            }
+            finally {
+                log.info("method: {} requestURI: {} contentType: {} response.status: {}",
+                        request.getMethod(), request.getRequestURI(), response.getContentType(), response.getStatus());
+            }
+        }
+        else {
+            filterChain.doFilter(request, response);
+        }
+
+
+    }
+}
